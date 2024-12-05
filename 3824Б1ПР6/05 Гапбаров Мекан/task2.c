fde0d38a73cd59226b42493950b5b368709066df
MekanGapbarov <mekangapbarov5@gmail.com>
2024-12-05 10:01:32 +0300

    Update task2.c

--- 3824Б1ПР6/05 Гапбаров Мекан/task2.c
+++ 3824Б1ПР6/05 Гапбаров Мекан/task2.c
@@ -2,5 +2,10 @@
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
+  int a, b;
+  scanf_s("%d", &a);
+  scanf_s("%d", &b);
+  int res = a * b;
+  printf("%d\n, UINT_MAX", res);
   return 0;
 }
